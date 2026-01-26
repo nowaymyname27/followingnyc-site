7e531d6a02881b13c121206dde5d9d5f0c46959a
Jose Ramirez <galock@bu.edu>
2026-01-26 17:49:40 -0500

    Cleaned up some test files

--- studio/schemaTypes/index.js
+++ studio/schemaTypes/index.js
@@ -11,7 +11,6 @@ import featured from "./featured";
 import testimonial from "./testimonial";
 import person from "./person";
 import aiCollection from "./aiCollection";
-import { videoContent } from "./videoContent";
 
 export const schemaTypes = [
   tag,
@@ -27,5 +26,4 @@ export const schemaTypes = [
   testimonial,
   person,
   aiCollection,
-  videoContent,
 ];
